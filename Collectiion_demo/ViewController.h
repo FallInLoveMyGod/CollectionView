2a0a0cfe0656aec997a1f3e36f41afd412502cca
qinong-tianyaoqi <1174465110@qq.com>
2017-06-23 12:54:31 +0800

    Initial Commit

---
+++ Collectiion_demo/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  Collectiion_demo
+//
+//  Created by 田耀琦 on 2017/6/23.
+//  Copyright © 2017年 田耀琦. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
---
+++ Collectiion_demo/main.m
@@ -0,0 +1,16 @@
+//
+//  main.m
+//  Collectiion_demo
+//
+//  Created by 田耀琦 on 2017/6/23.
+//  Copyright © 2017年 田耀琦. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import "AppDelegate.h"
+
+int main(int argc, char * argv[]) {
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+    }
+}
